0b0f690b7f11ea00037ecee782aa2d57d7063f02
Sundhar S G <sundhargopal462@outlook.com>
2023-05-12 14:34:12 +0530

    Sample REST App

---
+++ SampleRESTAppCD/src/main/java/com/example/SampleRESTAppCD/SampleRestAppCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.SampleRESTAppCD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleRestAppCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleRestAppCdApplication.class, args);
+	}
+
+}
